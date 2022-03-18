eaafcad26df78e0ef93f0d63d87e262039e6cf16
tnavikas77 <navikastomas@gmail.com>
2022-03-18 19:34:37 +0200

    Update openport.ps1

--- openport/openport.ps1
+++ openport/openport.ps1
@@ -1,8 +1,8 @@
-﻿#run script to open the port and check connection between servers
-#check server name in dns nslookup servername
+#run script to open the port and check connection between servers
+#check server name - nslookup -debug ops01.net
 #modify host file on server 1 and 2
-#telnet dns 8001 to connect
-#netstat -a to see connection
+#check connection telnet ops01.net 8001
+#see all ports netstat -a
 $port=8001
 $endpoint = new-object System.Net.IPEndPoint ([system.net.ipaddress]::any, $port)
 $listener = new-object System.Net.Sockets.TcpListener $endpoint
